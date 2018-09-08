34f9b912e6e1b70174f0f181c7ccf3b14edbc5f8
DutchGhost <kasper199914@gmail.com>
2018-09-08 14:19:29 +0200

    add localhost option to Cli, alter the handled_copy

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,28 @@ use tokio::net::{TcpListener, TcpStream};
 use tokio::prelude::*;
 
 use std::mem::drop;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::net::{IpAddr, SocketAddr};
 
 #[derive(Debug, StructOpt)]
+#[structopt(name = "SimpleProxy", about = "A simple, async proxy")]
 struct Cli {
+    /// Localhost, can be set to internal only.
     #[structopt(
-        short = "l",
-        long = "localport",
+        long = "localhost",
         parse(try_from_str),
-        default_value = "8080"
+        default_value = "0.0.0.0"
     )]
+    localhost: IpAddr,
+
+    /// The port the proxy should run on.
+    #[structopt(short = "l", parse(try_from_str), default_value = "8080")]
     localport: u16,
 
+    /// The host this server is a proxy to.
     #[structopt(short = "r", long = "remotehost", parse(try_from_str))]
     remotehost: IpAddr,
 
+    /// The port of the host this server should proxy to.
     #[structopt(short = "p", long = "remoteport", parse(try_from_str))]
     remoteport: u16,
 }
@@ -30,46 +37,36 @@ struct Cli {
 fn main() {
     let app = Cli::from_args();
 
-    let server_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), app.localport);
+    let server_addr = SocketAddr::new(app.localhost, app.localport);
     let host_addr = SocketAddr::new(app.remotehost, app.remoteport);
 
     let tcp = TcpListener::bind(&server_addr).unwrap();
 
-    // Iterate incoming connections
     let server = tcp
         .incoming()
         .for_each(move |tcp| {
             let proxy = proxy(tcp, host_addr).map(drop).map_err(|err| {
-                // <-- error from proxy to host
-                eprintln!("err {}", err);
+                eprintln!("Could not connect to host: {}", err);
             });
 
             tokio::spawn(proxy);
 
             Ok(())
         }).map_err(|err| {
-            // <-- error from incomming connections
-            eprintln!("server error {:?}", err);
+            eprintln!("An error occured while accepting connections: {}", err);
         });
 
     // Start the runtime and spin up the server
     tokio::run(server);
 }
 
-/// A wrapper function over io::copy. Transform's the Future returned by io::copy into a Future<Item= (), Error = ()>
-fn handled_copy<T, T2, T3, E, F>(future: F) -> impl Future<Item = (), Error = ()>
+/// A wrapper function over io::copy. Transform's the Future returned by io::copy into a Future<Item= (), Error = io::Error>
+fn copy<R, W>(reader: R, writer: W) -> impl Future<Item = (), Error = io::Error>
 where
-    T: std::fmt::Display,
-    E: std::fmt::Debug,
-    F: Future<Item = (T, T2, T3), Error = E>,
+    R: AsyncRead,
+    W: AsyncWrite,
 {
-    future
-        .map(|(n, _, _)| {
-            println!("wrote {} bytes", n);
-        }).map_err(|err| {
-            // <-- io::copy error
-            eprintln!("IO Error: {:?}", err);
-        })
+    io::copy(reader, writer).map(|(n, ..)| println!("wrote {} bytes", n))
 }
 
 /// Takes in an object that implements both AsyncRead and AsyncWrite, representing the proxy-server.
@@ -86,11 +83,11 @@ where
     TcpStream::connect(&host).and_then(move |stream| {
         let (host_reader, host_writer) = stream.split();
 
-        let sending = handled_copy(io::copy(server_reader, host_writer));
-        let receiving = handled_copy(io::copy(host_reader, server_writer));
+        let sending = copy(server_reader, host_writer);
+        let receiving = copy(host_reader, server_writer);
 
         let proxy = sending.select(receiving).map(drop).map_err(|(err, _)| {
-            eprintln!("Error: {:?}", err);
+            eprintln!("An error occured while servering the proxy: {:?}", err);
         });
 
         tokio::spawn(proxy);
